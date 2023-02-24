cd89e0a9857bd429f94bc2dde71302e4435b9362
Diogo Mondego dos Santos <dm_dossantos@hotmail.com>
2023-02-23 22:46:34 -0300

    altera rollup

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -7,7 +7,7 @@ import packageJson from "./package.json" assert { type: "json" };
 
 export default [
   {
-    input: "src/index.tsx",
+    input: "src/index.ts",
     output: [
       {
         file: packageJson.main,
